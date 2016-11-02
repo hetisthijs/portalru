adb37555adaa5187c2185579185237f199ed9014
Thijs <thijs.broenink@intellifi.nl>
2016-11-02 20:59:08 +0100

    Finish rooster

--- bing/Readme.txt
+++
@@ -1,9 +0,0 @@
-Mini Bing Search
-
-Search Bing right from your browser without leaving your current page.
-
-Mini Bing Search is a proof of concept extension. It loads Bing's mobile search page (http://m.bing.com) in a browser action popup.
-
-This extension was developed by Juliana Peña (http://julianapena.com).
-
-Code is available under the GPLv2 (http://creativecommons.org/licenses/GPL/2.0/).
--- js/popup.js
+++ js/popup.js
@@ -33,7 +33,21 @@ $(function() {
                 loadBlackboard();
                 break;
         }
-    }
+    };
+    
+    dayToNL = function(text, number) {
+        let today = new Date().getDate();
+        let eng = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+        let dutch = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag'];
+        if (number == today)
+            return 'vandaag';
+        if (number == today+1)
+            return 'morgen';
+        for (let i = 0; i < eng.length; i++) {
+            if (eng[i] === text)
+                return dutch[i];
+        }
+    };
     
     loadEmail = function() {
         chrome.runtime.sendMessage({
@@ -42,16 +56,27 @@ $(function() {
 		}, function(response) {
             $('#widgetEmail').html(response);
 		});
-    }
+    };
     
     loadRooster = function() {
         chrome.runtime.sendMessage({
 			action: 'xhttp',
 			url: "https://portal.ru.nl/nl/c/portal/render_portlet?p_l_id=6828682&p_p_id=roosterviewer_WAR_roosterviewerportlet",
 		}, function(response) {
-            $('#widgetRooster').html(response);
+            let content = '<table id="timetable">';
+            $(".event-item", response).each(function(index, element){
+                 let title = $('h4', this).html().split(' <span')[0] + '</a>';
+                 let dayText = $('.event-date-day-text', this).text().trim();
+                 let dayNumber = $('.event-date-day-number', this).text().trim();
+                 let time = $('.event-date-time', this).text()
+                 let location = $('.event-location', this).text();
+                 let description = $('.event-description', this).text();
+                 content += '<tr><td><b>'+ dayToNL(dayText, dayNumber) +'</b><br /><small>' + time + '</small></td><td><b>' + title + '</b><br /><small>' + location + ', ' + description + '</small></td>';
+            });
+            content += '</table>';
+            $('#widgetRooster').html(content);
 		});
-    }
+    };
 
     loadBlackboard = function(response) {
         chrome.runtime.sendMessage({
@@ -65,7 +90,7 @@ $(function() {
             });
             $('#widgetBlackboard').html(content);
 		});
-    }
+    };
     
     chrome.runtime.onMessage.addListener(function(request, sender, callback) {
         switch(request.action) {
